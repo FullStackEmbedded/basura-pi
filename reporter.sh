3beed671af5cea3cfa8fbf829891b69eee5bc9b9
Daniel Lee <daniel.lee@eumetsat.int>
2019-01-05 00:39:20 +0100

    Report fill states to Basura Server
    This is very hacky. First the unique trash can IDs are sorted out and
    reported to the server, ignoring the return code. Then the script
    attempts to report the fill states to the server. If successful, they'Re
    appended to the Reported Log.

--- reporter.sh
+++ reporter.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-# Read all lines from Fill State Log
+FILL_STATE_LOG=log/fill-state-log.csv
+REPORTED_LOG=log/reported-log.csv
+BASURA_SERVER=localhost:8000
+
+# Make sure Trash Can is registered with server
+for uuid in $(tail -n +2 $FILL_STATE_LOG | cut -d ';' -f 3 | sort -u); do
+  http POST $BASURA_SERVER/trashcans/ id=$uuid
+done
+
+# Report all lines from Fill State Log
+# By the way, this is *not* the right way to parse text, but we hacked it this
+# way to save time
 i=0
 while read line; do
+  success=0
   if [[ $i -gt 0 ]]; then  # Skip log header
     # Serialize each line as JSON
     echo $line | \
-      sed -e 's/;/", "time": "/' | \
-      sed -e 's/;/", "trash_can_uuid": "/' | \
-      sed -e 's/;/", "fill_state": /' | \
+      sed -e 's/;/", "timestamp": "/' | \
+      # This hack makes sure that we can set BASURA_SERVER to point at any address
+      sed -e 's/;/", "trash_can": "http:\/\/TRASH_CAN_ROOT\/trashcans\//' | \
+      sed "s/TRASH_CAN_ROOT/$BASURA_SERVER/" |\
+      # We need an extra slash here to access the right address
+      sed -e 's/;/\/", "fill_state": /' | \
       sed -e 's/^/{"uuid": "/' | \
-      sed -e 's/$/}/'
+      sed -e 's/$/}/' | \
+      http --check-status POST $BASURA_SERVER/trashstates/ && success=1
+    if [[ $success -eq 1 ]]; then
+      echo $line | cut -d \; -f 1 >> $REPORTED_LOG
+    else
+      echo FAIL: Could not register fill state with server.
+    fi
   fi
   i=$((i+1))
-done < log/fill-state-log.csv
+done < $FILL_STATE_LOG
+
+
