66f066a330913703fe7a38cca4558e46f3cdc335
cartux <ckoehler@gmx.de>
2019-01-05 13:02:08 +0100

    Added truncate_log which reads the reporter log and compares/deletes rows with existing uuids in fill state logs. Rep_filename added in settings.

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 
 LOG_DIR = "log/"
 LOG_FILENAME = "fill-state-log.csv"
-
+REP_FILENAME = "reported-log.csv"
 TRASH_CAN_INFORMATION_FILENAME = "trash-can-information.json"
 
 SCHEDULE_INTERVAL = 2  # seconds
